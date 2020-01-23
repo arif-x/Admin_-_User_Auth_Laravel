03a7ee1d9c1bfd601ca25506ff829442bdb91f80
Ariffudin Not Responding <42700729+Arif-X@users.noreply.github.com>
2020-01-23 20:28:41 +0700

    Update Admin Controller Guard Route

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', function(){
-    return view('admin');
-})->name('adminpage');
+Route::get('/admin', 'Auth\AdminLoginController@dashboard');
 Route::get('admin-login','Auth\AdminLoginController@showLoginForm');
 Route::post('admin-login', ['as' => 'admin-login', 'uses' => 'Auth\AdminLoginController@login']);
 Route::get('admin-register','Auth\AdminLoginController@showRegisterPage');
